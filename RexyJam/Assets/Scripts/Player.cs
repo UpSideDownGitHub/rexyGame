e0e4b15a55d0ebe94c8c0d81e9eebf05450a8268
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2024-01-15 13:03:24 +0000

    Player Movement & Shooting

--- RexyJam/Assets/Scripts/Player.cs
+++ RexyJam/Assets/Scripts/Player.cs
@@ -1,14 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float movementSpeed;
-    public bool thrust;
+    [Header("General")]
+    public Rigidbody2D rb;
+    public GameObject player;
+    public GameObject gun;
+    public GameObject firePoint;
+
+    [Header("Movement")]
+    public float rotationSpeed;
+    public float gunRotationSpeed;
     public float thrustForce;
 
-    private Vector2 _movementVec;
+    [Header("Shooting")]
+    public GameObject bullet;
+    public float fireForce;
 
-    public void OnMovement(InputAction.CallbackContext ctx) => _movementVec = ctx.ReadValue<Vector2>();
+
+    // INPUT
+    [SerializeField]private float _lookVec;
+    [SerializeField] private float _aimVec;
+    [SerializeField] private bool _thrust;
+    [SerializeField] private bool _fire;
+
+    public void OnLeftStick(InputAction.CallbackContext ctx) => _lookVec = ctx.ReadValue<float>();
+    public void OnRightStick(InputAction.CallbackContext ctx) => _aimVec = ctx.ReadValue<float>();
+    public void OnThrust(InputAction.CallbackContext ctx) => _thrust = ctx.action.WasPressedThisFrame();
+    public void OnFire(InputAction.CallbackContext ctx) => _fire = ctx.action.WasPressedThisFrame();
+
+    public void Update()
+    {
+        rb.angularVelocity = 0;
+        player.transform.Rotate(new Vector3(0,0, -rotationSpeed * _lookVec));
+        gun.transform.Rotate(new Vector3(0, 0, -gunRotationSpeed * _aimVec));
+
+        if (_thrust)
+        {
+            rb.AddForce(player.transform.up * thrustForce); 
+        }
+
+        if (_fire)
+        {
+            _fire = false;
+            var tempBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+            tempBullet.GetComponent<Rigidbody2D>().AddForce(tempBullet.transform.up * fireForce);
+        }
+    }
 }
