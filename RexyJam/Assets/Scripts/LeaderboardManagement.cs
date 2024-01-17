ef33cd40172d367da2738dc910003c89483d8d20
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2024-01-17 01:48:31 +0000

    Leaderboard keyboard

--- RexyJam/Assets/Scripts/LeaderboardManagement.cs
+++ RexyJam/Assets/Scripts/LeaderboardManagement.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
 using TMPro;
-
-// NOTE: Make sure to include the following namespace wherever you want to access Leaderboard Creator methods
 using Dan.Main;
 using Dan;
-using UnityEngine.SocialPlatforms.Impl;
 using Dan.Models;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEditor.Search;
 
 public class LeaderboardManagement : MonoBehaviour
 {
+    [Header("Keyboard")]
+    public GameObject Cog;
+    public string[] alphabet = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+    public string currentName;
+    public TMP_Text[] letters;
+    public GameObject[] selectionIndicators;
+
+    public float rotationSpeed;
+    private float _lookVec;
+    private float _lookVecRex;
+
+    public void OnLeftStickRexy(InputAction.CallbackContext ctx) => _lookVecRex = ctx.ReadValue<float>();
+    public void OnLeftStickOther(InputAction.CallbackContext ctx) => _lookVec = ctx.ReadValue<float>();
+    public void OnDelete(InputAction.CallbackContext ctx)
+    {
+        if (ctx.action.WasPerformedThisFrame())
+            DeletePressed();
+    }
+    public void OnContinue(InputAction.CallbackContext ctx)
+    {
+        if (ctx.action.WasPerformedThisFrame())
+            ContinuePressed();
+
+    }
+    public void OnSelect(InputAction.CallbackContext ctx)
+    {
+        if (ctx.action.WasPerformedThisFrame())
+            SelectPressed();
+    }
+
+    [Header("Leaderboard")]
     public TMP_Text[] entryTextObjects;
     public TMP_Text previousScoreText;
     public TMP_InputField usernameInputField;
@@ -16,8 +47,74 @@ public class LeaderboardManagement : MonoBehaviour
     private void Start()
     {
         LoadEntries();
-        GetPersonalEntry();
     }
+
+    void Update()
+    {
+        if (_lookVecRex != 0)
+        {
+            var val = _lookVecRex;
+            if (_lookVecRex < 0)
+                val = -(1 - Mathf.Abs(_lookVecRex));
+            else
+                val = 1 - val;
+            Cog.transform.Rotate(new Vector3(0, 0, -rotationSpeed * val));
+        }
+        else
+        {
+            Cog.transform.Rotate(new Vector3(0, 0, -rotationSpeed * _lookVec));
+        }
+
+    }
+
+    public void DisableIndicators()
+    {
+        selectionIndicators[0].SetActive(false);
+        selectionIndicators[1].SetActive(false);
+        selectionIndicators[2].SetActive(false);
+    }
+
+    public void DeletePressed()
+    {
+        if (currentName.Equals(""))
+            return;
+        DisableIndicators();
+        letters[currentName.Length - 1].text = "";
+        selectionIndicators[currentName.Length - 1].SetActive(true);
+        currentName = currentName.Remove(currentName.Length - 1);
+        return;
+    }
+
+    public void ContinuePressed()
+    {
+        // save the score to the leaderboard with the name being sent
+        if (currentName.Equals(""))
+            return;
+    }
+
+    public void SelectPressed()
+    {
+        // rotation per == 360/26
+        // area for each is 
+        // (360/26 * i) - 360/52 <-> (360/26 * i) + 360/52   
+        // to find the letter get current rotation
+        for (int i = 0; i < 26; i++)
+        {
+            if (Cog.transform.rotation.eulerAngles.z >= (360f / 26f * i) - 360f / 52f &&
+                Cog.transform.rotation.eulerAngles.z <= (360f / 26f * i) + 360f / 52f)
+            {
+                if (currentName.Length > 2)
+                    return;
+                currentName += alphabet[i];
+                letters[currentName.Length - 1].text = alphabet[i];
+                DisableIndicators();
+                if (currentName.Length > 2)
+                    return;
+                selectionIndicators[currentName.Length].SetActive(true);
+                return;
+            }
+        }
+    } 
 
     private void LoadEntries()
     {
@@ -43,18 +140,5 @@ public class LeaderboardManagement : MonoBehaviour
         {
             print("Error Uploading");
         });
-    }
-
-    public void GetPersonalEntry()
-    {
-        Leaderboards.RexyGameJamLeaderboard.GetPersonalEntry(OnPersonalEntryLoaded, errored =>
-        {
-            print("Error getting peronal");
-        });
-    }
-
-    private void OnPersonalEntryLoaded(Entry entry)
-    {
-        previousScoreText.text = $"{entry.RankSuffix()}. {entry.Username} : {entry.Score}";
     }
 }
